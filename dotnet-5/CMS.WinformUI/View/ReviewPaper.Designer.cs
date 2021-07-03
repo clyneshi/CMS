132275cd552924a1e07fa9d6ef2d26ed88637e00
Freer Clyne <freerclyne@live.com>
2021-07-03 16:38:36 +1000

    refactor review paper page UI logic

--- dotnet-5/CMS.WinformUI/View/ReviewPaper.Designer.cs
+++ dotnet-5/CMS.WinformUI/View/ReviewPaper.Designer.cs
@@ -28,68 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataGridView_paperReview = new System.Windows.Forms.DataGridView();
+            this.btn_rate = new System.Windows.Forms.Button();
+            this.btn_download = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_paperReview)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 12);
-            this.dataGridView1.MultiSelect = false;
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1040, 493);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView_paperReview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView_paperReview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_paperReview.Location = new System.Drawing.Point(3, 12);
+            this.dataGridView_paperReview.MultiSelect = false;
+            this.dataGridView_paperReview.Name = "dataGridView1";
+            this.dataGridView_paperReview.ReadOnly = true;
+            this.dataGridView_paperReview.RowHeadersVisible = false;
+            this.dataGridView_paperReview.RowTemplate.Height = 33;
+            this.dataGridView_paperReview.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView_paperReview.Size = new System.Drawing.Size(1040, 493);
+            this.dataGridView_paperReview.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(299, 552);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(224, 65);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Rate";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_rate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_rate.Location = new System.Drawing.Point(299, 552);
+            this.btn_rate.Name = "button1";
+            this.btn_rate.Size = new System.Drawing.Size(224, 65);
+            this.btn_rate.TabIndex = 1;
+            this.btn_rate.Text = "Rate";
+            this.btn_rate.UseVisualStyleBackColor = true;
+            this.btn_rate.Click += new System.EventHandler(this.btn_rate_Click);
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(3, 550);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(242, 65);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Download";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btn_download.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_download.Location = new System.Drawing.Point(3, 550);
+            this.btn_download.Name = "button2";
+            this.btn_download.Size = new System.Drawing.Size(242, 65);
+            this.btn_download.TabIndex = 2;
+            this.btn_download.Text = "Download";
+            this.btn_download.UseVisualStyleBackColor = true;
+            this.btn_download.Click += new System.EventHandler(this.btn_download_Click);
             // 
             // ReviewPaper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1294, 804);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_download);
+            this.Controls.Add(this.btn_rate);
+            this.Controls.Add(this.dataGridView_paperReview);
             this.Name = "ReviewPaper";
             this.Text = "ReviewPaper";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_paperReview)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView_paperReview;
+        private System.Windows.Forms.Button btn_rate;
+        private System.Windows.Forms.Button btn_download;
     }
 }
--- dotnet-5/CMS.WinformUI/View/ReviewPaper.cs
+++ dotnet-5/CMS.WinformUI/View/ReviewPaper.cs
@@ -1,5 +1,4 @@
-﻿using CMS.DAL.Models;
-using CMS.BL.Services.Interface;
+﻿using CMS.BL.Services.Interface;
 using System;
 using System.IO;
 using System.Windows.Forms;
@@ -8,7 +7,7 @@ namespace CMS
 {
     public partial class ReviewPaper : Form
     {
-        readonly IPaperService _paperService;
+        private readonly IPaperService _paperService;
         private readonly IApplicationStrategy _applicationStrategy;
 
         public ReviewPaper(IPaperService paperService, IApplicationStrategy applicationStrategy)
@@ -22,61 +21,56 @@ namespace CMS
 
         public void Init()
         {
-            //paperid = 0;
-            var reviewPapers = _paperService.GetPapersForReview(_applicationStrategy.GetLoggedInUserInfo().User.Id, _applicationStrategy.GetLoggedInUserInfo().ConferenceId.Value);
-            dataGridView1.DataSource = reviewPapers;
+            var paperReviews = _paperService.GetPapersForReview(_applicationStrategy.GetLoggedInUserInfo().User.Id, _applicationStrategy.GetLoggedInUserInfo().ConferenceId.Value);
+            dataGridView_paperReview.DataSource = paperReviews;
             //dataGridView1.Columns[5].Visible = false;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn_rate_Click(object sender, EventArgs e)
         {
             if (RatingBox.Show() == DialogResult.Yes)
             {
                 // TODO: validation
-                if (RatingBox.Rating != 0 && dataGridView1.CurrentRow.Index >= 0)
-                    _paperService.UpdatePaperRating(RatingBox.Rating, (int)dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells["paperId"].Value);
+                var index = dataGridView_paperReview.CurrentRow.Index;
+                if (RatingBox.Rating != 0 && index >= 0)
+                    _paperService.UpdatePaperRating(RatingBox.Rating, (int)dataGridView_paperReview.Rows[index].Cells["paperId"].Value);
             }
             Init();
         }
 
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        private void btn_download_Click(object sender, EventArgs e)
         {
-            //if (e.RowIndex >= 0)
-            //    paperid = (int)dataGridView1.Rows[e.RowIndex].Cells["paperId"].Value;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog sfd = new SaveFileDialog();
-            if (dataGridView1.RowCount > 0 && dataGridView1.CurrentRow.Index >= 0)
+            var saveDialog = new SaveFileDialog();
+            var index = dataGridView_paperReview.CurrentRow.Index;
+            if (index >= 0)
             {
-                int paperid = (int)dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells["paperId"].Value;
-                Paper paper = _paperService.GetPaperById(paperid);
-                sfd.FileName = paper.FileName;
+                int paperId = (int)dataGridView_paperReview.Rows[index].Cells["paperId"].Value;
+                var paper = _paperService.GetPaperById(paperId);
+                saveDialog.FileName = paper.FileName;
                 if (paper.Format.ToUpper().Equals(".PDF"))
                 {
-                    sfd.Filter = "Data Files PDF|*.pdf";
-                    sfd.DefaultExt = "pdf";
-                    sfd.AddExtension = true;
+                    saveDialog.Filter = "Data Files PDF|*.pdf";
+                    saveDialog.DefaultExt = "pdf";
+                    saveDialog.AddExtension = true;
                 }
                 if (paper.Format.ToUpper().Equals(".DOC"))
                 {
-                    sfd.Filter = "Data Files WORD|*.doc";
-                    sfd.DefaultExt = "doc";
-                    sfd.AddExtension = true;
+                    saveDialog.Filter = "Data Files WORD|*.doc";
+                    saveDialog.DefaultExt = "doc";
+                    saveDialog.AddExtension = true;
                 }
                 if (paper.Format.ToUpper().Equals(".TXT"))
                 {
-                    sfd.Filter = "Data Files Text Files|*.txt";
-                    sfd.DefaultExt = "txt";
-                    sfd.AddExtension = true;
+                    saveDialog.Filter = "Data Files Text Files|*.txt";
+                    saveDialog.DefaultExt = "txt";
+                    saveDialog.AddExtension = true;
                 }
 
-                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                if (saveDialog.ShowDialog() == DialogResult.OK)
                 {
-                    BinaryWriter bw = new BinaryWriter(File.Create(sfd.FileName));
-                    bw.Write(paper.Content);
-                    bw.Dispose();
+                    var binaryWriter = new BinaryWriter(File.Create(saveDialog.FileName));
+                    binaryWriter.Write(paper.Content);
+                    binaryWriter.Dispose();
                 }
             }
         }
